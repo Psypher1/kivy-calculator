223a7a3cb460655d6e0ed18185f0e14b304d7d7c
dante <jamesmidzi@gmail.com>
2024-05-06 17:04:30 +0200

    feat: move render to root class

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 kivy.require("1.9.0")
 
 
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+
 class PyCalculator(App):
     def build(self):
-        return Label(text="Py Calc")
+        return MyRoot()
 
 
 if __name__ == "__main__":
