427fd8864f42f0b2ffe0125786b5f7aef47bf9fd
dante <jamesmidzi@gmail.com>
2024-05-06 17:49:25 +0200

    feat: buttons on app aand window size

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 
-kivy.require("1.9.0")
+Window.size = (400, 500)
 
 
 class MyRoot(BoxLayout):
 
     def __init__(self):
         super(MyRoot, self).__init__()
+
+    def calc_symbol(self, symbol):
+        self.calc_field.text += symbol
 
 
 class PyCalculator(App):
